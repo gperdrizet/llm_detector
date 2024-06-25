760053e129db3db96b570b08d2ce3c036398ee80
George Perdrizet <george@perdrizet.org>
2024-06-25 15:45:39 -0400

    Added functions to initialize Celery app and Flask app with Celery.

--- llm_detector/functions/flask_app.py
+++ llm_detector/functions/flask_app.py
@@ -3,11 +3,15 @@
 import time
 from typing import Callable
 from flask import Flask, request # type: ignore
-from celery import Celery, Task
+from celery import Celery, Task # type: ignore
+# pylint: disable=W0223
 
-def celery_init_app(app: Flask) -> Celery:
-    '''Sets up Celery app for Flask'''
+def create_celery_app(app: Flask) -> Celery:
+    '''Sets up Celery app object'''
+
     class FlaskTask(Task):
+        '''Gives task function an active Flask context'''
+
         def __call__(self, *args: object, **kwargs: object) -> object:
             with app.app_context():
                 return self.run(*args, **kwargs)
@@ -16,7 +20,25 @@ def celery_init_app(app: Flask) -> Celery:
     celery_app.config_from_object(app.config["CELERY"])
     celery_app.set_default()
     app.extensions["celery"] = celery_app
+
     return celery_app
+
+def create_flask_celery_app() -> Flask:
+    '''Creates flask app for use with Celery'''
+
+    app = Flask(__name__)
+
+    app.config.from_mapping(
+        CELERY=dict(
+            broker_url="redis://localhost",
+            result_backend="redis://localhost",
+            task_ignore_result=True,
+        ),
+    )
+    app.config.from_prefixed_env()
+    create_celery_app(app)
+
+    return app
 
 def setup(input_queue: Callable, output_queue: Callable) -> Callable:
     '''Define the flask app'''
