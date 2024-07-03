b6355b6e5a104a5429d7e2f7a005f01a2705041c
George Perdrizet <george@perdrizet.org>
2024-07-03 11:07:27 -0400

    Fixed module naming conflict.

--- benchmarking/__main__.py
+++ benchmarking/__main__.py
@@ -1,7 +1,7 @@
 '''Main function to run jobs based on command line arguments'''
 
 import benchmarking.functions.helper as helper_funcs
-import benchmarking.functions.benchmarking as benchmark_funcs
+import benchmarking.functions.benchmark as benchmark_funcs
 import benchmarking.functions.binoculars as binocular_funcs
 
 if __name__ == "__main__":
