c5a9e5f6702f3c88a018386538cdbc5db84d589d
George Perdrizet <george@perdrizet.org>
2024-07-12 13:29:30 -0400

    Added authentication to Redis docker container via environment variables.

--- api/configuration.py
+++ api/configuration.py
@@ -28,7 +28,10 @@ CLEAR_LOGS=True
 # Flask app stuff
 HOST_IP=os.environ['HOST_IP']
 FLASK_PORT=os.environ['FLASK_PORT']
-REDIS_URL=f"redis://{os.environ['REDIS_IP']}"
+REDIS_IP=os.environ['REDIS_IP']
+REDIS_PORT=os.environ['REDIS_PORT']
+REDIS_PASSWORD=os.environ['REDIS_PASSWORD']
+REDIS_URL=f'redis://:{REDIS_PASSWORD}@{REDIS_IP}:{REDIS_PORT}'
 
 # Models to use for scoring
 READER_MODEL='meta-llama/Meta-Llama-3-8B'
