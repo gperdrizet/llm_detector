21a909b56de7c691ec3688abcf195784a07eb6b6
George Perdrizet <george@perdrizet.org>
2024-07-12 22:35:12 -0400

    Got toy example bot working with echo functionality and one slash command.

--- telegram_bot/__main__.py
+++ telegram_bot/__main__.py
@@ -1,18 +1,45 @@
 '''Main module to run the telegram bot'''
 
 import os
-import asyncio
-import telegram
+import logging
+from telegram import Update # pylint: disable=import-error
+from telegram.ext import filters, MessageHandler, ApplicationBuilder, CommandHandler, ContextTypes
 
-async def main():
-    '''Main async bot function'''
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
 
-    bot = telegram.Bot(os.environ['TELEGRAM_TOKEN'])
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    '''Listens for start messages'''
 
-    async with bot:
-        print(await bot.get_me())
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    '''Echos user messages'''
+
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id, text=update.message.text)
+
+
+async def caps(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    '''Echos user messages sent with caps command back in all caps'''
+
+    text_caps = ' '.join(context.args).upper()
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id, text=text_caps)
 
 if __name__ == '__main__':
 
-    asyncio.run(main())
+    application = ApplicationBuilder().token(os.environ['TELEGRAM_TOKEN']).build()
+    start_handler = CommandHandler('start', start)
+    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
+    caps_handler = CommandHandler('caps', caps)
+
+    application.add_handler(start_handler)
+    application.add_handler(echo_handler)
+    application.add_handler(caps_handler)
+
+    application.run_polling()
