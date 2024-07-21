c7d4ec794cc22adae16c23668a56920b8b4dd775
George Perdrizet <george@perdrizet.org>
2024-07-21 11:32:34 -0400

    Started working on feature engineering pipeline, combined scored Hans 2024 text fragments into single dataframe for input.

---
+++ classifier/configuration.py
@@ -0,0 +1,17 @@
+'''Project specific constants and configuration.'''
+
+import os
+
+######################################################################
+# Project meta-stuff: paths, logging, etc. ###########################
+######################################################################
+
+# Get path to this config file so that we can define
+# other paths relative to it
+PROJECT_ROOT_PATH = os.path.dirname(os.path.realpath(__file__))
+
+# Other project paths
+DATA_PATH = f'{PROJECT_ROOT_PATH}/data'
+BENCHMARKING_DATA_PATH = f'{DATA_PATH}/benchmarking'
+HANS_DATA_PATH = f'{DATA_PATH}/hans_2024'
+COMBINED_SCORED_HANS_DATA = f'{HANS_DATA_PATH}/all-scores.json'
