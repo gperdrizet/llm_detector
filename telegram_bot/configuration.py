e68479575deb379027446b631307e806a90cdcdf
George Perdrizet <george@perdrizet.org>
2024-08-04 08:37:38 -0400

    Added saving plot with timestamped filename.

--- telegram_bot/configuration.py
+++ telegram_bot/configuration.py
@@ -24,4 +24,5 @@ HOST_IP = os.environ['HOST_IP']
 FLASK_PORT = os.environ['FLASK_PORT']
 
 # Bot greeting, set at start of new conversation
-BOT_GREETING = "Hi, I'm a bot! I can't talk to you, but if you send me some text, I'll try to determine if it was written by a human or a machine."
+BOT_GREETING = """Hi, I'm a bot! I can't talk to you, but if you send me some text,\
+      I'll try to determine if it was written by a human or a machine."""
