b30ed74ef95cda1046f710b5694779afdb138b78
George Perdrizet <george@perdrizet.org>
2024-11-12 16:39:04 -0500

    Finished API Docker image.

--- api/build_api_image.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker build \
---build-arg host_ip=$HOST_IP \
---build-arg flask_port=$FLASK_PORT \
---build-arg redis_ip=$REDIS_IP \
---build-arg redis_port=$REDIS_PORT \
--t gperdrizet/agatha:api .
