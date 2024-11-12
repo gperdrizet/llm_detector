9450722e09f52396971870365a9334ae821b215d
George Perdrizet <george@perdrizet.org>
2024-11-11 22:44:16 -0500

    Renamed build helper scripts.

--- api/build_api_image.sh
+++ api/build_api_image.sh
@@ -5,6 +5,6 @@ docker build \
 --build-arg flask_port=$FLASK_PORT \
 --build-arg redis_ip=$REDIS_IP \
 --build-arg redis_port=$REDIS_PORT \
---build-arg redis_password=$REDIS_PASSWORD \
---build-arg hf_token=$HF_token \
+--secret redis_password=$REDIS_PASSWORD \
+--secret hf_token=$HF_token \
 -t gperdrizet/agatha:api .
