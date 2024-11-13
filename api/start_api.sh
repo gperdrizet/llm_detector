aa740a0c9c5ee58bc0f108c62697664aa19cbfef
George Perdrizet <george@perdrizet.org>
2024-11-12 23:11:46 -0500

    Fixed HuggingFace authentication bug.

--- api/start_api.sh
+++ api/start_api.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Authenticate session to HuggingFace so we can download gated models if needed
-python -c "from huggingface_hub.hf_api import HfFolder; HfFolder.save_token($HF_TOKEN)"
+python3 -c "from huggingface_hub.hf_api import HfFolder; HfFolder.save_token('$HF_TOKEN')"
 
 # Start the API with Gunicorn
 gunicorn -w 1 --bind $HOST_IP:$FLASK_PORT 'api:flask_app'
